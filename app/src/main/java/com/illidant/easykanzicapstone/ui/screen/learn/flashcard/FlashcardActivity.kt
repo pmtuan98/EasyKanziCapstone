4779b2096db74144cfa01fa74d6aaff8e95189c7
AoTuTap <thinhnpse98@gmail.com>
2020-07-17 18:03:58 +0700

    Kanji fill data

--- app/src/main/java/com/illidant/easykanzicapstone/ui/screen/learn/flashcard/FlashcardActivity.kt
+++ app/src/main/java/com/illidant/easykanzicapstone/ui/screen/learn/flashcard/FlashcardActivity.kt
@@ -1,8 +1,9 @@
 package com.illidant.easykanzicapstone.ui.screen.learn.flashcard
 
 import android.os.Bundle
-import android.widget.Button
-import android.widget.TextView
+import android.view.MotionEvent
+import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.illidant.easykanzicapstone.R
 import com.illidant.easykanzicapstone.domain.model.Vocabulary
@@ -15,11 +16,46 @@ import kotlinx.android.synthetic.main.activity_flashcard.*
 import kotlinx.android.synthetic.main.flashcard_layout_back.*
 import kotlinx.android.synthetic.main.flashcard_layout_front.*
 
+
 class FlashcardActivity : AppCompatActivity(), LearnContract.View {
+    private var x1 = 0f
+    private  var x2:kotlin.Float = 0f
+    val MIN_DISTANCE = 150
+    var counter = 0
+    val kanjiList : MutableList<String> = mutableListOf()
+    val hiraganaList : MutableList<String> = mutableListOf()
+    val meaningList : MutableList<String> = mutableListOf()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_flashcard)
         initialize()
+        flash_card.setOnTouchListener(object : View.OnTouchListener {
+            override fun onTouch(v: View?, event: MotionEvent?): Boolean {
+                when (event!!.action) {
+                    MotionEvent.ACTION_DOWN -> x1 = event.x
+                    MotionEvent.ACTION_UP -> {
+                        x2 = event.x
+                        val deltaX = x2 - x1
+                        if (Math.abs(deltaX) > MIN_DISTANCE) {
+                            // Left to Right swipe action
+                            if (x2 > x1) {
+                                Toast.makeText(v!!.context, "Left to Right swipe [Next]", Toast.LENGTH_SHORT)
+                                    .show()
+                                previous()
+                            } else {
+                                Toast.makeText(v!!.context, "Right to Left swipe [Previous]", Toast.LENGTH_SHORT)
+                                    .show()
+                                next()
+                            }
+                        } else {
+                            // consider as something else - a screen tap for example
+                        }
+                    }
+                }
+
+                return v?.onTouchEvent(event) ?: true
+            }
+        })
 
 
     }
@@ -44,10 +80,7 @@ class FlashcardActivity : AppCompatActivity(), LearnContract.View {
     }
 
     override fun getVocabByLessonID(listVocab: List<Vocabulary>) {
-        var counter = 0
-        val kanjiList : MutableList<String> = mutableListOf()
-        val hiraganaList : MutableList<String> = mutableListOf()
-        val meaningList : MutableList<String> = mutableListOf()
+
         // Add to list
         for (vocab in listVocab) {
             kanjiList.add(vocab.kanji_vocab)
@@ -65,26 +98,33 @@ class FlashcardActivity : AppCompatActivity(), LearnContract.View {
 
         // Next button click
         btn_flashcard_next.setOnClickListener {
-            counter++
-            if (counter == kanjiList.size) {
-                counter = 0
-            }
-            flashcard_kanji.text = kanjiList[counter]
-            flashcard_meaning.text = meaningList[counter]
-            flashcard_hira.text = hiraganaList[counter]
-            tv_questionNo.text = (counter + 1).toString()
+            next()
         }
 
         //Previous button click
         btn_flashcard_previous.setOnClickListener {
-            counter--
-            if (counter < 0) {
-                counter = kanjiList.size - 1
-            }
-            flashcard_kanji.text = kanjiList[counter]
-            flashcard_meaning.text = meaningList[counter]
-            flashcard_hira.text = hiraganaList[counter]
-            tv_questionNo.text = (counter + 1).toString()
+            previous()
         }
     }
+    fun previous(){
+        counter--
+        if (counter < 0) {
+            counter = kanjiList.size - 1
+        }
+        flashcard_kanji.text = kanjiList[counter]
+        flashcard_meaning.text = meaningList[counter]
+        flashcard_hira.text = hiraganaList[counter]
+        tv_questionNo.text = (counter + 1).toString()
+    }
+    fun next() {
+        counter++
+        if (counter == kanjiList.size) {
+            counter = 0
+        }
+        flashcard_kanji.text = kanjiList[counter]
+        flashcard_meaning.text = meaningList[counter]
+        flashcard_hira.text = hiraganaList[counter]
+        tv_questionNo.text = (counter + 1).toString()
+    }
+
 }
