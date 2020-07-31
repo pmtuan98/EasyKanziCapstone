7f894fa6d2224cddf2ba53d24627f84f1d462256
pmtuan98 <pmtuan98@gmail.com>
2020-07-31 18:25:12 +0700

    Fix layout

--- app/src/main/java/com/illidant/easykanzicapstone/ui/screen/profile/test_history/TestHistoryActivity.kt
+++ app/src/main/java/com/illidant/easykanzicapstone/ui/screen/profile/test_history/TestHistoryActivity.kt
@@ -1,11 +1,13 @@
 package com.illidant.easykanzicapstone.ui.screen.profile.test_history
 
 import android.content.Context
+import android.content.Intent
 import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import androidx.recyclerview.widget.GridLayoutManager
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.illidant.easykanzicapstone.R
 import com.illidant.easykanzicapstone.domain.model.TestHistory
 import com.illidant.easykanzicapstone.platform.api.RetrofitService
@@ -13,8 +15,12 @@ import com.illidant.easykanzicapstone.platform.repository.SearchRepository
 import com.illidant.easykanzicapstone.platform.repository.TestRepository
 import com.illidant.easykanzicapstone.platform.source.remote.SearchRemoteDataSource
 import com.illidant.easykanzicapstone.platform.source.remote.TestRemoteDataSource
+import com.illidant.easykanzicapstone.ui.screen.home.HomeActivity
+import com.illidant.easykanzicapstone.ui.screen.ranking.RankingActivity
+import com.illidant.easykanzicapstone.ui.screen.search.SearchActivity
 import com.illidant.easykanzicapstone.ui.screen.search.SearchAdapter
 import com.illidant.easykanzicapstone.ui.screen.search.SearchPresenter
+import kotlinx.android.synthetic.main.activity_profile.*
 import kotlinx.android.synthetic.main.activity_search.*
 import kotlinx.android.synthetic.main.activity_test_history.*
 
@@ -25,6 +31,7 @@ class TestHistoryActivity : AppCompatActivity(), TestHistoryContract.View {
         setContentView(R.layout.activity_test_history)
         val prefs: SharedPreferences = getSharedPreferences("com.illidant.kanji.prefs", Context.MODE_PRIVATE)
         val userID = prefs.getInt("userID", 0)
+        configViews()
         historyPresenter.getTestHistoryRequest(userID)
     }
 
@@ -38,5 +45,47 @@ class TestHistoryActivity : AppCompatActivity(), TestHistoryContract.View {
     override fun onTestHistoryData(listHistory: List<TestHistory>) {
         recyclerHistory!!.layoutManager = GridLayoutManager(this, 1)
         recyclerHistory!!.adapter = TestHistoryAdapter(listHistory,this)
+    }
+
+    private fun configViews() {
+        //set bottom navigation bar
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+
+
+        //set home selected
+        bottomNavigationView.selectedItemId =
+            R.id.profile
+
+
+        //Perform ItemSelectedListener
+        bottomNavigationView.setOnNavigationItemSelectedListener(BottomNavigationView.OnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.home -> {
+                    startActivity(Intent(applicationContext, HomeActivity::class.java))
+                    finish()
+                    overridePendingTransition(0, 0)
+                    return@OnNavigationItemSelectedListener true
+
+                }
+                R.id.search -> {
+                    startActivity(Intent(applicationContext, SearchActivity::class.java))
+                    finish()
+                    overridePendingTransition(0, 0)
+                    return@OnNavigationItemSelectedListener true
+
+                }
+                R.id.ranking -> {
+                    startActivity(Intent(applicationContext, RankingActivity::class.java))
+                    finish()
+                    overridePendingTransition(0, 0)
+                    return@OnNavigationItemSelectedListener true
+
+                }
+                R.id.profile -> {
+                    return@OnNavigationItemSelectedListener true
+                }
+            }
+            false
+        })
     }
 }
