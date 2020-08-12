55b7dcc93759793106054324114dd6b652acb5a6
pmtuan98 <pmtuan98@gmail.com>
2020-08-12 17:38:25 +0700

    Chang attribute of test history model

--- app/src/main/java/com/illidant/easykanzicapstone/domain/model/TestHistory.kt
+++ app/src/main/java/com/illidant/easykanzicapstone/domain/model/TestHistory.kt
@@ -6,9 +6,9 @@ data class TestHistory (
     @SerializedName("name")
     val levelName: String,
     @SerializedName("timeTaken")
-    val timeTaken: String,
+    val timeTaken: Int,
     @SerializedName("resultPoint")
-    val resultPoint: String,
+    val resultPoint: Int,
     @SerializedName("dateAttend")
     val dateAttend: String
 )
