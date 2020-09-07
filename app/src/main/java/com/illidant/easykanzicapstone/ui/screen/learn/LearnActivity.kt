5abb15010b2bc10f9b525342423afe0c30e4ac3f
pmtuan98 <pmtuan98@gmail.com>
2020-09-07 23:59:09 +0700

    Fix multiple choice when dont have quiz data

--- app/src/main/java/com/illidant/easykanzicapstone/ui/screen/learn/LearnActivity.kt
+++ app/src/main/java/com/illidant/easykanzicapstone/ui/screen/learn/LearnActivity.kt
@@ -3,27 +3,41 @@ package com.illidant.easykanzicapstone.ui.screen.learn
 import android.content.Intent
 import android.os.Bundle
 import androidx.recyclerview.widget.GridLayoutManager
+import cn.pedant.SweetAlert.SweetAlertDialog
 import com.illidant.easykanzicapstone.BaseActivity
 import com.illidant.easykanzicapstone.R
+import com.illidant.easykanzicapstone.domain.model.Quiz
 import com.illidant.easykanzicapstone.domain.model.Vocabulary
 import com.illidant.easykanzicapstone.platform.api.RetrofitService
+import com.illidant.easykanzicapstone.platform.repository.QuizRepository
 import com.illidant.easykanzicapstone.platform.repository.VocabularyRepository
+import com.illidant.easykanzicapstone.platform.source.remote.QuizRemoteDataSource
 import com.illidant.easykanzicapstone.platform.source.remote.VocabularyRemoteDataSource
 import com.illidant.easykanzicapstone.ui.screen.learn.flashcard.FlashcardActivity
 import com.illidant.easykanzicapstone.ui.screen.learn.multiple_choice.MultipleChoiceActivity
 import com.illidant.easykanzicapstone.ui.screen.learn.writing.WritingActivity
+import com.illidant.easykanzicapstone.ui.screen.quiz.QuizContract
+import com.illidant.easykanzicapstone.ui.screen.quiz.QuizPresenter
 import kotlinx.android.synthetic.main.activity_learn.*
 
-class LearnActivity : BaseActivity(), LearnContract.View {
+class LearnActivity : BaseActivity(), LearnContract.View, QuizContract.View {
 
     private var lessonId: Int = 0
     private var lessonName: String = ""
-    
+    private val mutualListQuiz: MutableList<Quiz> = mutableListOf()
+
     private val presenter by lazy {
         val retrofit = RetrofitService.getInstance(application).getService()
         val remote = VocabularyRemoteDataSource(retrofit)
         val repository = VocabularyRepository(remote)
         LearnPresenter(this, repository)
+    }
+
+    private val quizPresenter by lazy {
+        val retrofit = RetrofitService.getInstance(application).getService()
+        val remote = QuizRemoteDataSource(retrofit)
+        val repository = QuizRepository(remote)
+        QuizPresenter(this, repository)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -37,6 +51,7 @@ class LearnActivity : BaseActivity(), LearnContract.View {
         lessonId = intent.getIntExtra("LESSON_ID", 0)
         lessonName = intent.getStringExtra("LESSON_NAME")
         presenter.vocabByLessonRequest(lessonId)
+        quizPresenter.quizByLessonRequest(lessonId)
     }
 
     private fun configViews() {
@@ -54,9 +69,15 @@ class LearnActivity : BaseActivity(), LearnContract.View {
             startActivity(intent)
         })
         multipleMethod.setOnClickListener({
-            val intent = Intent(it.context, MultipleChoiceActivity::class.java)
-            intent.putExtra("LESSON_ID", lessonId)
-            startActivity(intent)
+            if(mutualListQuiz.isEmpty()){
+                val errDialog = SweetAlertDialog(this, SweetAlertDialog.ERROR_TYPE)
+                errDialog.contentText = "No quiz data."
+                errDialog.show()
+            }else{
+                val intent = Intent(it.context, MultipleChoiceActivity::class.java)
+                intent.putExtra("LESSON_ID", lessonId)
+                startActivity(intent)
+            }
         })
 
         btnBack.setOnClickListener {
@@ -72,6 +93,14 @@ class LearnActivity : BaseActivity(), LearnContract.View {
         tvTotalVocab.text = "(${listVocab.size})"
         recyclerViewVocab!!.layoutManager = GridLayoutManager(this, 1)
         recyclerViewVocab.adapter = LearnVocabAdapter(this, listVocab)
+    }
+
+    override fun getQuizByLessonID(listQuiz: List<Quiz>) {
+        mutualListQuiz.addAll(listQuiz)
+    }
+
+    override fun getQuizByLevelID(listQuiz: List<Quiz>) {
+        //Not use
     }
 
 }
